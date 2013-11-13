ad1265f65ea6877d729a395a5a73921d86248be4
engz <realengz@gmail.com>
2013-11-12 16:21:30 -0800

    Delete data folder
    Start adding popover functionality

--- js/action.js
+++ js/action.js
@@ -1,3 +1,4 @@
 $(document).ready(function() {
     $('[data-toggle="tooltip"]').tooltip({'placement': 'top'});
+    $('[data-toggle="popover"]').popover({'placement': 'right'});
 }); //END $(document).ready()
