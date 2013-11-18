f1197eacdd9b469865b32e33f66ca985f17b67b8
Katlyn Edwards <katlyn.edwards@hotmail.com>
2013-11-18 15:27:58 -0800

    Adding "+" button - not perfect, adds below instead of to the right :C

--- js/directives.js
+++ js/directives.js
@@ -1,38 +1,44 @@
 angular.module('twitterCrisis', [])
 // Controller
-    .controller('Ctrl', function($scope) {
-	var tweets = getTweets();
-	console.log("HI!");
-	console.log(tweets);
-	$scope.tweets = tweets;
+    .controller('paneCtrl', function($scope) {
+		var tweets = getTweets();
+		$scope.tweets = tweets;
     })
+
+    .controller('leftMenuCtrl', function($scope, $compile) {
+    	$scope.addColumn = function() {
+    		var el = $compile("<column-stream>")($scope);
+    		$(".container").append(el);
+    	}
+    })
+
 
 
 // Directives
     .directive("columnStream", function() {
-	return {
-	    restrict: 'EA',
-	    templateUrl: 'pane.html'
-	};
+		return {
+		    restrict: 'EA',
+		    templateUrl: 'pane.html'
+		};
     })
-    .directive("leftMenu", function() {
 
-	return {
-	    restrict: 'EA',
-	    templateUrl: 'leftMenu.html'
-	};
+    .directive("leftMenu", function() {
+		return {
+		    restrict: 'EA',
+		    templateUrl: 'leftMenu.html'
+		};
     })
 
     .directive("tweet", function() {
-	return {
-	    restrict: 'EA',
-	    templateUrl: 'tweet.html',
-	    link: function(scope, element, attrs) {
-		attrs.$observe('tweet', function(tweet) {
-		    scope.tweet = tweet;
-		});
-	    }
-	};
+		return {
+		    restrict: 'EA',
+		    templateUrl: 'tweet.html',
+		    link: function(scope, element, attrs) {
+			attrs.$observe('tweet', function(tweet) {
+			    scope.tweet = tweet;
+			});
+		    }
+		};
     });
 
 
