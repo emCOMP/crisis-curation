969e871cab4f6c7203a88f2a8cdce0b6d3c988f2
engz <realengz@gmail.com>
2014-01-09 17:19:13 -0800

    Delete all files - restart git repo

--- js/action.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-    $('[data-toggle="tooltip"]').tooltip({'placement': 'top'});
-    $('[data-toggle="popover"]').popover({'placement': 'right'});
-}); //END $(document).ready()
