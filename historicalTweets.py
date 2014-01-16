16626353b6a043999a6e707ead0c8d50059a1290
Camille Cobb <cobbc12@gmail.com>
2014-01-15 18:53:04 -0800

    Same code, just added TODOs in comments for Tridiv to automatically check that there is a datetime-format entry for created_at time of all tweets (so that we can do greater than/less than operations in this code).

--- historicalTweets.py
+++ historicalTweets.py
@@ -3,18 +3,32 @@ import pymongo
 import datetime
 import time
 import dateutil.parser as dparser
+import bson
 
-HISTORICAL_DB_NAME = 'crisisdb'
+HISTORICAL_DB_NAME = 'newdb'  # 'crisisdb'
 LIVE_DB_NAME = 'current_database'
 TIME_MULTIPLIER = 1
 SLEEP_SECONDS = 5
 
+# Twitter API-specific field names
+CREATED_AT = 'created_at'
+UNIQUE_ID = 'uuid'
+
+############## TODO: Tridiv
+# Our created_at datetime object field name 
+OUR_CREATED_AT = 'helloworld'
 
 # connect to two databases
 # be sure to run mongod in the terminal before starting
 dbclient = MongoClient('localhost', 27017)
 historicaldb = dbclient[HISTORICAL_DB_NAME] 
 currentdb = dbclient[LIVE_DB_NAME]
+
+
+############## TODO: Tridiv - deal with datetime crap
+# look through all entries in historicaldb for time, turn them into datetime, store in "OUR_CREATED_AT" field
+# change all future CREATED_AT lookups to look into OUR_CREATED_AT
+##############
 
 # record current time
 previousTime = datetime.datetime.now()
@@ -24,8 +38,12 @@ previousTime = datetime.datetime.now()
 # relies on us having changed all times in the db to be datetime objects
 # see ipython notebook for how to update these to datetime objects
 # explain next loc
-first_tweet = historicaldb.tweets.find().sort("uuid", pymongo.ASCENDING).limit(1)[0]
-starting_time = first_tweet['createdAt']
+first_tweet = historicaldb.tweets.find().sort(UNIQUE_ID, pymongo.ASCENDING).limit(1)[0]
+print 'HELLLLLO'
+#print bson.loa(first_tweet)
+print first_tweet
+# TODO real twitter api uses created_at and might have other datetime issues to deal with
+starting_time = first_tweet[CREATED_AT]
 historicalTime = starting_time
 
 # infinite loop
@@ -44,7 +62,7 @@ while True:
     previousTime = timeNow
 
     # run DB query to get all tweets within delta of historicalTime, going forward
-    new_tweets = historicaldb.tweets.find({'createdAt': {"$gte": historicalTime, "$lt": historicalTime+(delta * TIME_MULTIPLIER)}})
+    new_tweets = historicaldb.tweets.find({CREATED_AT: {"$gte": historicalTime, "$lt": historicalTime+(delta * TIME_MULTIPLIER)}})
     
     # update historicalTime
     historicalTime += (delta * TIME_MULTIPLIER)
