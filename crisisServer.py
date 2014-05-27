0dccb2810425913002bf47235c6534ecf988851e
tridiv <katestarbird@d-128-208-219-139.dhcp4.washington.edu>
2014-05-27 13:33:16 -0700

    paste server stuff

--- crisisServer.py
+++ crisisServer.py
@@ -591,4 +591,4 @@ userTags = UserTagsModel()
 
 # ---- Starting the Server ----
 
-run(host=SERVER_HOST, port=SERVER_PORT, debug=True)
+run(server ='paste', host=SERVER_HOST, port=SERVER_PORT, debug=True)
